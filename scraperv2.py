0cfef5ac403b50e347e9817609c90baf84f9562c
dylanflynn477 <dylanflynn477@gmail.com>
2023-04-03 16:14:55 -0400

    Global variables

---
+++ scraperv2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Declaring global variables
+global url
+global options
+
+
+url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,0022-2380,AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+    driver.get(url)
+    print("Page URL:", driver.current_url)
+    print("Page Title:", driver.title)
+    #elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result')
+    elements = driver.find_elements(By.TAG_NAME, 'prm-search-result-list')
+    print(f"Results: {elements}")
