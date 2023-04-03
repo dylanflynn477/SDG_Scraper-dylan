8d83025dba071538e0bf5dd67fcff0a4f09ca7c9
dylanflynn477 <dylanflynn477@gmail.com>
2023-04-03 16:26:37 -0400

    global variable w

--- scraperv2.py
+++ scraperv2.py
@@ -6,7 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 # Declaring global variables
 global url
 global options
-
+global journal_list
+global query_results
 
 url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,0022-2380,AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
 options = webdriver.ChromeOptions()
@@ -16,5 +17,5 @@ with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), op
     print("Page URL:", driver.current_url)
     print("Page Title:", driver.title)
     #elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result')
-    elements = driver.find_elements(By.TAG_NAME, 'prm-search-result-list')
+    elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result-container')
     print(f"Results: {elements}")
