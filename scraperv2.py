a9186642262a9309a8b3502976eca88d722f1a50
dylanflynn477 <dylanflynn477@gmail.com>
2023-04-03 20:36:51 -0400

    bad results :(

--- scraperv2.py
+++ scraperv2.py
@@ -8,28 +8,54 @@ import pandas as pd
 global url
 global options
 global journal_list
+global journal_issn_list
+global journal_eissn_list
+global journal
 global query_results
 global csvfile
 
 csvfile = pd.read_csv('scopusjournals.csv')
 
 journal_list = []
+journal_issn_list = []
+journal_eissn_list = []
+query_results = []
+journal = ""
 
 for i in range(len(csvfile)):
     journal_list.append(csvfile.iat[i,0])
+    journal_issn_list.append(csvfile.iat[i,1])
+    journal_eissn_list.append(csvfile.iat[i,2])
+#print(journal_list)
 
-url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,0022-2380,AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
-with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
-    driver.get(url)
-    print("Page URL:", driver.current_url)
-    print("Page Title:", driver.title)
-    #elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result')
-    elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result-container')
-    print(f"Results: {elements}")
-    if elements == []:
-        print("Journal ")
+def query_journal(url):
+    with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+        driver.get(url)
+        print("Page URL:", driver.current_url)
+        print("Page Title:", driver.title)
+        #elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result')
+        elements = driver.find_elements(By.TAG_NAME, 'prm-brief-result-container')
+        print(f"Results: {elements}")
+        if elements == []:
+            print(f"Journal {journal} not found.")
+            return 0
+        else:
+            print(f"Journal {journal} successfully found.")
+            return 1
 
-#for i in range(len(csvfile)):
-#    url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,' + journal_list[i] + ',AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
+for i in range(len(csvfile)):
+    #if str(journal_issn_list[i]) == "nan":
+    #    if str(journal_eissn_list[i]) == "nan":
+    #        pass
+    url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,' + str(journal_issn_list[i]) + ',AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
+    journal = journal_list[i]
+    if query_journal(url) == 0:
+        url = 'https://sju.primo.exlibrisgroup.com/discovery/search?query=issn,contains,' + str(journal_eissn_list[i]) + ',AND&tab=Everything&search_scope=MyInst_and_CI&vid=01USCIPH_INST:SJU&mode=advanced&offset=0'
+        if query_journal(url) == 0:
+            query_results.append("No")
+        else:
+            query_results.append("Yes")
+    else:
+        query_results.append("Yes")
