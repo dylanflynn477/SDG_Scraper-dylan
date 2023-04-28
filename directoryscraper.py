a0eb0c545c55f8340b6989dac62756f5d9e87b32
dylanflynn477 <dylanflynn477@gmail.com>
2023-04-28 18:58:00 -0400

    bruh

---
+++ directoryscraper.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+from selenium.webdriver.common.action_chains import ActionChains as action
+import time 
+import math
+import os
+
+options = webdriver.ChromeOptions()
+prefs = {"download.default_directory" : "C:/Users/dylan/Desktop/temp"}
+options.add_experimental_option("prefs", prefs)
+#options.add_argument("--headless=new")
+
+url = 'https://ssb.sju.edu/pls/PRODSSB/hzgkcdir.P_DirSearchNest'
+
+def wait(seconds=5):
+    time.sleep(seconds)
+
+#User login: //*[@id="UserID"]
+#Password: //*[@id="PIN"]
+#Directory button = /html/body/form/table/tbody/tr[1]/td[1]
+#Submit = /html/body/div[3]/form/p/input
+#Email = //*[@id="i0116"]
+#Next = //*[@id="idSIButton9"]
+#Pwd= //*[@id="i0118"]
+#Submit = //*[@id="idSIButton9"]
+#Verify = //*[@id="idDiv_SAOTCS_Proofs"]/div/div/div/div[2]/div
+#2FACode = //*[@id="idTxtBx_SAOTCC_OTC"]
+#Verifybutton = //*[@id="idSubmit_SAOTCC_Continue"]
+#Univdirectbutton = //*[@id="block-nestmain"]/ul/li[3]/a
+#Opens a new tab
+# driver.switch_to.window(driver.window_handles[1])
+
+def __init__():
+    with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options) as driver:
+        def click(object):
+            action(driver).move_to_element(object).click(object).perform()
+        driver.get(url)
+        time.sleep(5.0)
+        ## SJU Credentials are required to access the student directory. This portion of the code handles the grueling sign-in process.
+        print("SJU Credentials Required.")
+        print("Username:")
+        username = input()
+        # Username must not contain @sju.edu
+        username = username.rstrip('@sju.edu')
+        print("Password:")
+        password = input()
+        usernamebox = driver.find_element(By.XPATH, '//*[@id="UserID"]')
+        passwordbox = driver.find_element(By.XPATH, '//*[@id="PIN"]')
+        usernamebox.send_keys(username)
+        passwordbox.send_keys(password)
+        submit = driver.find_element(By.XPATH, '//html/body/div[3]/form/p/input')
+        click(submit)
+        wait()
+        email = driver.find_element(By.XPATH, '//*[id="i0116]')
+        email.send_keys(username + '@sju.edu')
+        next = driver.find_element(By.XPATH, '//*[@id="idSIButton9"]')
+        click(next)
+        passwordbox = driver.find_element(By.XPATH, '//*[@id="i0118"]')
+        passwordbox.send_keys(password)
+        next = driver.find_element(By.XPATH, '//*[@id="idSIButton9"]')
+        click(next)
+        wait()
+        verifybox = driver.find_element(By.XPATH, '//*[@id="idDiv_SAOTCS_Proofs"]/div/div/div/div[2]/div')
+        click(verifybox)
+        wait()
+        twofacode = driver.find_element(By.XPATH, '//*[@id="idTxtBx_SAOTCC_OTC"]')
+        print("A verification code is required to access the directory. Enter the 6-digit code here:")
+        verificationcode = input()
+        twofacode.send_keys(verificationcode)
+        submitverification = driver.find_element(By.XPATH, '//*[@id="idSubmit_SAOTCC_Continue"]')
+        click(submitverification)
+        wait()
+        directorybutton = driver.find_element(By.XPATH, '//html/body/form/table/tbody/tr[1]/td[1]')
+        click(directorybutton)
+        wait()
+        student = driver.find_element(By.XPATH, '//html/body/form/table/tbody/tr[1]/td[1]/select/option[2]')
+        click(student)
+        maxhitsbutton = driver.find_element(By.XPATH,'//html/body/form/table/tbody/tr[3]/td[1]/select')
+        click(maxhitsbutton)
+        onehundred = driver.find_element(By.XPATH, '//html/body/form/table/tbody/tr[3]/td[1]/select/option[4]')
+        click(onehundred)
+        search = driver.find_element(By.XPATH, '//html/body/form/pre/input')
+        click(search)
+        wait()
+__init__()
